b107dd7d04a95cbef36de7d1491e1c4ba2a9f49a
vikipretium <vighneshpasupathy@gmail.com>
2022-06-28 20:28:17 +0530

    set up registration and sign in form with devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,7 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
+ 
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -67,4 +68,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name)
     end
+  
 end
