61c0344021c6dc77cd0c564dfad1d62e7c836f18
Saied Gaffer <saied2421998@gmail.com>
2022-06-29 11:15:26 +0200

    set up recipe and index view of the recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_110635) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_080711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name", null: false
+    t.time "prepration_time", null: false
+    t.time "cooking_time", null: false
+    t.text "description"
+    t.boolean "public", default: true
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_110635) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
