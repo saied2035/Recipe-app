7d718ab92e62b7a8e38cc2c14760242a788ac586
Saied Gaffer <saied2421998@gmail.com>
2022-07-02 17:27:57 +0200

    finish authorization

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_111702) do
   create_table "foods", force: :cascade do |t|
     t.string "name", null: false
     t.string "unit"
-    t.float "price"
+    t.decimal "price"
     t.integer "quantity"
     t.bigint "user_id"
     t.datetime "created_at", null: false
